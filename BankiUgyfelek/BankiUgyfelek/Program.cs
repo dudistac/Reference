4378dbfaf4dcb4d94fd3776662eae0869ad697f1
dudistac <49782118+dudistac@users.noreply.github.com>
2020-01-29 16:30:00 +0100

    Add files via upload

---
+++ BankiUgyfelek/BankiUgyfelek/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BankiUgyfelek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ma kettő ügyfél jelentkezett be. Adataik a következőek:
+            Ugyfel ElsoUgyfel = new Ugyfel("Gaspar Elek", 26,"+36/70-123-4567", "Budapest", 11900000, 507125.00, 75000.00, "Ervenyes");
+            Ugyfel MasodikUgyfel = new Ugyfel("Bármi Áron", 15, "Nincs", "Debrecen", 11900001, 52400, 10000, "Érvényes");
+
+            //Első ügyfelet érdekli a nyílvántartott adatai és 10000 Ft-nyi hitelt szeretne felvenni 4 évre 6 %-os kamattal, ha lehetséges akkor kíváncsi a törlesztőrészletre
+            ElsoUgyfel.UgyfelAdatok();
+            ElsoUgyfel.HitelFelvetel(10000);
+            Console.WriteLine("A törlesztőrészlet az " + Ugyfel.TorlesztoReszlet(10000, 48, 0.06) + " Ft havonta.");
+            ElsoUgyfel.fLekerdezes();
+
+            //Második ügyfél afelől érdeklődik, hogy milyen típusú a számlája. Vehet-e fel hitelt. Illetve fel szeretne tölteni 2500 Ft-ot a számlájára.
+            MasodikUgyfel.fSzamlatipus();
+            MasodikUgyfel.lehetEHitel();
+            MasodikUgyfel.fBefizetes(2500);
+            MasodikUgyfel.fLekerdezes();
+
+            //Az első ügyfélnek hány nap múlva nem maradna pénze ha naponta 50000 Ft-ot költene?
+            int napokSzama = 0;
+
+            while (ElsoUgyfel.egyenleg > 0)
+            {
+                ElsoUgyfel.egyenleg -= 50000;
+                napokSzama++;
+            }
+            Console.WriteLine(napokSzama+ " nap múlva fogy majd el a pénze.");
+
+            //A ma bejelentkezett ügyfeleket számlaszám alapján eltároljuk és kiírjuk.
+            List<int> maiUgyfelek = new List<int>();
+            maiUgyfelek.Add(ElsoUgyfel.szamlaszam);
+            maiUgyfelek.Add(MasodikUgyfel.szamlaszam);
+            for (int j = 0; j < 2; j++) 
+            {
+                Console.WriteLine(maiUgyfelek[j]);
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BankiUgyfelek/BankiUgyfelek/Ugyfel.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankiUgyfelek
+{[Serializable]
+    class Ugyfel
+    {
+
+
+        //Ügyfelek adatai
+        public string nev;
+        public int kor;
+        public string elerhetoseg;
+        public string lakhely;
+        public int szamlaszam;
+        public double egyenleg;
+        public double maxOsszeg; //A maximálisan felvehető összeg egy tranzakció folyamán
+        private string szamlatipus;
+        
+
+        //A konsztruktor
+        public Ugyfel(string uNev, int uKor, string uElerhetoseg, string uLakhely, int uSzamlaszam, double uEgyenleg, double uMaxOsszeg, string uSzamlatipus)
+        {
+            nev = uNev;
+            kor = uKor;
+            elerhetoseg = uElerhetoseg;
+            lakhely = uLakhely;
+            szamlaszam = uSzamlaszam;
+            egyenleg = uEgyenleg;
+            maxOsszeg = uMaxOsszeg;
+            Szamlatipus = uSzamlatipus;
+            
+        }
+
+        //A számlatípus kor alapján határozódik meg
+        public string Szamlatipus
+        {
+            get { return szamlatipus; }
+            set {
+                if (kor < 24 && kor > 14)
+                {
+                    szamlatipus = "Diákszámla";
+                }
+                else if (kor>=24) 
+                {
+                    szamlatipus = "Érvényes";
+                }
+                else
+                {
+                    szamlatipus = "Érvénytelen";
+                }
+            }
+        }
+
+        //Kiírja az ügyfél legfontosabb adatait
+        public void UgyfelAdatok() 
+        {
+            Console.WriteLine(String.Format("Az ügyfél neve: {0}, a lakhelye: {1}, a számlaszáma: {2}.",nev, lakhely, szamlaszam));
+        }
+
+        //Befizetés a számlára
+        public void fBefizetes(double BefOsszeg) 
+        {
+            this.egyenleg += BefOsszeg;    
+        }
+
+        //Egyenleg lekérdezése
+        public void fLekerdezes() 
+        {
+            Console.WriteLine("Az egyenleg: " + egyenleg + " Ft.");
+        }
+
+        //Pénz felvétel számláról
+        public double fPenzFelvetel(double LevOsszeg) 
+        {
+            if (egyenleg < LevOsszeg)
+            {
+                Console.WriteLine("Nem vehető le az előírt pénzösszeg, mert nincs elég pénz a számlán!");
+                return egyenleg;
+            }else { 
+                 //egyenleg -= LevOsszeg;
+                Console.WriteLine("A tranzakció sikeresen végrehajtva!");
+                return egyenleg-=LevOsszeg;
+            }
+        }
+
+            
+
+        //Adott esetben a hitelfelvétel feltétele az, hogy az ügyfélnek idősebbnek kell lennie 18-nál
+        //ebből aódódan a számlatípusának érvényesnek kell lennie,
+        //a biztos anyagi hátteret pedig a számlán található pénzösszeg mennyisége határozza meg
+        public bool lehetEHitel() 
+        {
+            if (egyenleg >= 100000 && Szamlatipus=="Érvényes" && kor>=18) {
+                Console.WriteLine("Hitel felvétele lehetséges!");
+                return true;
+            }
+            Console.WriteLine("Az ügyfél nem vehet fel hitelt!");
+            return false;
+        }
+
+        //Ebben az esetben a hitelvelvétel feltétele, hogy az igényelt összeg nem lehet nagyobb az egyenleg 2.5-szörösénél
+        public void HitelFelvetel(double igenyeltOsszeg) 
+        {
+            if (lehetEHitel() == true && igenyeltOsszeg <= egyenleg * 2.5)
+            {
+                egyenleg += igenyeltOsszeg;
+                Console.WriteLine("Az összeg sikeresen felvéve!");
+            }
+            else 
+            { 
+                Console.WriteLine("Az összeg felvétele sikertelen!");
+            }
+            
+        }
+
+        //A törlesztőrészlet számításánál a futamidőt hónapokban kell megadni, a kamatlábat kettő tizedesjegyig.
+        public static double TorlesztoReszlet(double felvettOsszeg, int futamido, double kamatláb)
+        {
+            double kamatlab = kamatláb / 12;
+            double annuitasFaktor = 1 / kamatlab - (1 / (kamatlab * Math.Pow(1 + kamatlab, futamido)));
+            double torlesztoReszlet = felvettOsszeg / annuitasFaktor;
+            return torlesztoReszlet;
+        }
+
+        public void fSzamlatipus()
+        {
+            Console.WriteLine(szamlatipus);
+        }
+        
+
+
+    }
+}
